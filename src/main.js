4c4122e7d7947681bc25eaa7257bed9f6dc7d90e
scscgogogo <20456992@qq.com>
2022-06-04 16:02:55 +0800

    创建了web服务器<Express>

--- src/main.js
+++ src/main.js
@@ -1,23 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((request, response) => {
-  const data = {
-    id: 1,
-    title: '关山月',
-    content: '明月出天山，苍茫云海间',
-  };
-
-  const jsonData = JSON.stringify(data);
-
-  response.writeHead(200, {
-    'Content-Type': 'application/json; charset=utf-8',
-  });
-
-  response.write(jsonData);
-
-  response.end();
+app.listen(port, () => {
+  console.log('🚀 服务已启动！');
 });
 
-server.listen(3000, () => {
-  console.log('🚀 服务已启动！');
+app.get('/', (request, response) => {
+  response.send('你好');
 });
