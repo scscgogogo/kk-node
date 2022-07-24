83866d8fe699bd574e2f6779fa374fc7f1a4f48d
scscgogogo <20456992@qq.com>
2022-07-24 12:52:51 +0800

    理解中间件

--- src/post/post.router.ts
+++ src/post/post.router.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import * as postController from '../post/post.controller';
-
+import { requestUrl } from '../app/app.middleware';
 const router = express.Router();
 
 /**
  * 内容列表
  */
-router.get('/posts', postController.index);
+router.get('/posts', requestUrl, postController.index);
 
 /**
  * 导出路由
