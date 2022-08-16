1ab4eff3071b74492977e17f4582600d606e5125
scscgogogo <20456992@qq.com>
2022-08-16 15:58:50 +0800

    可以跨域访问

--- src/app/Index.ts
+++ src/app/Index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import postRounter from '../post/post.router';
 import userRouter from '../user/user.router';
 import authRouter from '../auth/auth.router';
@@ -7,6 +8,11 @@ import { defaultErrorHandler } from './app.middleware';
  * 创建应用
  */
 const app = express();
+
+/**
+ * 跨域资源共享
+ */
+app.use(cors({ origin: '*', exposedHeaders: 'X-Total-Count' }));
 
 /**
  * 处理 JSON
